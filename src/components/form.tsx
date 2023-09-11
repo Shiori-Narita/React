c2f0ed349476b3f9120d1179d265b69d778e0505
Shiori Narita <shiori.narita9093@gmail.com>
2023-09-11 16:22:40 +0900

    コメントアウト完了

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -40,22 +40,38 @@ export default function Formtext() {
 //モジュール内から1つだけエクスポートすることが可能で、他のモジュールからこのモジュールをインポートした場合デフォルトエクスポートが自動的に割り当てられる。
 //今回はFormtextという関数を宣言している。以下が関数の内容である。
 //Q：今回classではなくfunctionにした理由は何？(日報の質問事項にて質問済み)
+//A:Reactにはクラスコンポーネントと関数コンポーネントというものがあり、今回関数コンポーネントで書かれていたためclassからfunctionに変更をし対応した。
+//　そもそもクラスコンポーネントと関数コンポーネントでは書き方が違っているため、下記サイトを参照し確認する事。
+//クラスコンポーネント：Reactの古典的な方法でjsのクラスを使って定義される。render()メソッドを用いてUIを返す。stateやlifecycleメソッドを利用することが可能。
+//関数コンポーネント：シンプルで簡潔な構文でコンポーネントを定義する。通常はアロー関数を使って定義される。stateやlifecycleメソッドを利用しない代わりにReactHooksを使って同様の機能を使える。
+//参考サイト：https://qiita.com/omo_taku/items/18da0c020672a368f166
   const [zipcode, setZipcodeMain] = useState<Zipcode>({
     main: "",
     sub: ""
   });
+  //zipcodeは状態変数で現在の郵便番号のデータを保持する。
+  //setZipcodeMain は、この状態変数を更新するための関数。setZipcodeMain を呼び出すことで、zipcode の値を変更できる。
+  //main: "",sub: ""はzipcodeの初期値として扱う。""を使って初期化されている。
   const [address, setAddress] = useState<Address>({
     address1: "",
     address2: "",
     address3: ""
   });
+  //addressは状態変数で現在の住所のデータを保持する。
+  //setAddressは、この状態変数を更新するための関数。setAddressを呼び出すことで、address の値を変更できる。
+  //address1: "",address2: "",address3: ""はaddressの初期値として扱う。""を使って初期化されている。
 
   const updateZipcodeMain = (e: ChangeEvent<HTMLInputElement>) => {
     setZipcodeMain({ ...zipcode, main: e.target.value });
   };
+  //...:スプレッド構文【反復可能な要素や関数呼び出しに展開することができる構文】
   const updateZipcodeSub = async (e: ChangeEvent<HTMLInputElement>) => {
     setZipcodeMain({ ...zipcode, sub: e.target.value });
+      //...:スプレッド構文【反復可能な要素や関数呼び出しに展開することができる構文】
     if (e.target.value.length === 4 && zipcode.main.length === 3) {
+      //右側に4文字入力できている、かつ左側に3文字入力できている場合→tryが実行される。
+      //try catchはワンセット。API関係などではほぼ使われる。tryで実行できないと判断されたものはcatchの実行へと移る。
+      //if elseだけではダメなのか？→APIはいつ更新されるか分からないエラーの宝庫のため、例外処理(try)を行っておく方がいい。
       try {
         const res = await axios.get(
           "https://zipcloud.ibsnet.co.jp/api/search",
@@ -65,6 +81,7 @@ export default function Formtext() {
             }
           }
         );
+        //上記URLより、入力された7桁に完全一致するもののデータを抽出する(get)
         if (res.data.results) {
           const result = res.data.results[0];
           setAddress({
@@ -74,9 +91,11 @@ export default function Formtext() {
             address3: result["address3"]
           });
         }
+        //抽出されたデータをそれぞれの項目へと配置する。(配列使用)
       } catch {
         alert("住所の取得に失敗しました。");
       }
+      //tryの処理が行われない場合はこちらが動作する。
     }
   };
         return (
